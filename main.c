80f2d14f5d96f65283236c67cf8d551364436c83
xavierad <30500440+xavierad@users.noreply.github.com>
2020-03-04 12:58:53 +0000

    Add files via upload

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-
+#include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <sys/types.h>
@@ -9,24 +9,82 @@
 #include <string.h>
 
 
+char cmd[255] = {'\0'};
+char *token[3];
+
 char *ip;
 char *port;
 
+/******************************** DÚVIDAS ********************************
+
+1- comando dkt após correr o executável, certo?--> acho que não, acho que o comando deve ser ./dkt <ip> <port>, como está feito no que está comentado
+2-
+
+*/
 
 int main(int argc, char *argv[]) {
+  /*
+  if(argc != 3) {
 
-  if(argc != 3) { // comando: dkt <ip> <port>
-    write(1, "Invocação da aplicação mal feita! Para invocá-la use: dkt <ip> <port>\n",
-      strlen("Invocação da aplicação mal feita! Para invocá-la use: dkt <ip> <port>"));
-    exit(0);
-  }
+    //write(1, "Invocação da aplicação mal feita! Para invocá-la use: dkt <ip> <port>\n",
+    //  strlen("Invocação da aplicação mal feita! Para invocá-la use: dkt <ip> <port>"));
+      printf("Invocação da aplicação mal feita! Para invocá-la use: dkt <ip> <port>\n");
+      exit(0);
+    }
   else {
     ip = argv[1]; port = argv[2];
-    write(1,"Introduzido: ", 13);
-    for (int i=0; i<strlen(*argv + i); i++) {
-      write(1, *(argv + i), strlen(*(argv + i))); write(1, " ", 1);
+    //write(1,"Introduzido: ", 13);
+    printf("Introduzido: ");
+    int i;
+    for (i=0; i<strlen(*argv + i); i++) {
+      //write(1, *(argv + i), strlen(*(argv + i))); write(1, " ", 1);
+      printf("%s ", *(argv + i));
+    }
+    printf("\n");
+  }
+  */
+
+  // validating the comand: dkt <ip> <port>
+  while(1) {
+    fgets(cmd, 255, stdin);
+
+    token[0] = strtok(cmd, " ");
+    int i;
+    for(i=1; i<=2; i++) {
+      token[i] = strtok(NULL, " ");
+      printf("\ntoken %s\n", token[i]);
+    }
+
+
+    if(strcmp(token[0], "dkt") != 0 || token[0] == NULL || token[1][0] == '\n' || token[1] == NULL ||
+      token[2] == NULL || token[2][0] == '\n') printf("\nThe command must be in format 'dkt <ip> <port>'\n");
+    else {
+      printf("\nApplication initialized!\n");
+      // getting the ip and port parameters
+      ip = token[1]; port = token[2];
+      printf("\nIP addr.: %s\nPORT: %s\n", ip, port);
+      break;
     }
   }
+
+  // application loop
+  while(strcmp(cmd, "exit\n") != 0){
+    memset(cmd, '\0', sizeof(cmd));
+
+    if(fgets(cmd, 255, stdin)){
+
+      if(strcmp(cmd, "exit\n") == 0) printf("You closed the application!\n\n");
+      else{
+
+
+        // validating the commands
+        //if(strcmp())
+      }
+    }
+
+
+  }
+
 
   return 0;
 }
