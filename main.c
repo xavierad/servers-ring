8ba8e690d46c9cbed03792bfc07d9832beb35ff6
xavierad <xdias_28@hotmail.com>
2020-03-07 15:30:19 +0000

    Up

--- main.c
+++ main.c
@@ -9,7 +9,7 @@
 
 1- comando dkt após correr o executável, certo?--> acho que não, acho que o comando deve ser ./dkt <ip> <port>, como está feito no que está comentado
 2- É preciso fazer uma verificação para o porto e IP? Se sim, é porque têm um formato próprios, certo?
-3- No comando entry, todos os argumentos são números? boot boot.IP boot.TCP
+3-
 4-
 
 ****************************************************************************/
@@ -54,6 +54,33 @@ int checkInteger(char *num){
 }
 
 
+int checkCommands(int nArgs, char* token){
+  int i = 0;
+  int isInt = 1;
+  char *args[nArgs];
+  int server = 0; //mudar o tipo para char caso for necessário
+
+  // splitting the command until it's NULL, validates it, counts the
+  //number of args, checks integer
+  for ( ;token = strtok(NULL, " "); token != NULL){
+
+    if(token[0] == '\n') break;
+    else {
+      i++;
+
+      args[i-1] = token;
+      if(!checkInteger(args[i-1])) isInt = 0;
+      printf("%d\n", isInt);
+    }
+  }
+  // number of arguments comparison and validating the required integer
+  if(i > nArgs || i < nArgs) return 0;
+  else if(!isInt) {
+    printf("There is something wrong in server number!\n");
+    return 0;
+  }
+  else return 1;
+}
 
 
 
@@ -87,72 +114,34 @@ int main(int argc, char *argv[]) {
 
       // validating the commands
       if(strncmp(token, "new", 3) == 0){
-        int nArgs = 1;
-        int i = 0;
-        int isInt = 0;
-        int server = 0; //mudar o tipo para char caso for necessário
 
-        // splitting the command until it's NULL, validates it, counts the
-        //number of args, checks integer
-        for ( ;token = strtok(NULL, " "); token != NULL){
-
-          if(token[0] == '\n') break;
-          else {
-            i++;
-            // whenever there is or not the number of required args, it checks
-            //whether after 'new' there is integers
-            isInt = checkInteger(token);
-            server = atoi(token);
-          }
-        }
-
-        // number of arguments comparison and validating the required integer
-        if(i > nArgs || i < nArgs) printf("Did you mean something like 'new <i>'?\n");
-        else if(!isInt) printf("There is no server number!\n");
+        if(!checkCommands(1, token)) printf("Did you mean something like 'new <i>'?\n");
         else {
           //do ring creation stuff here...
 
-          printf("New ring with server %d was created!\n", server);
+          printf("New ring was created!\n");
         }
       }
 
       else if(strncmp(token, "entry", 5) == 0){
-        int nArgs = 3;
-        int i = 0;
-        int isInt = 0;
-        char *args[nArgs];
-
-        // splitting the command until it's NULL, validates it, counts the
-        //number of args, checks integer
-        for ( ;token = strtok(NULL, " "); token != NULL){
-
-          if(token[0] == '\n') break;
-          else {
-
-            // change if boot boot.IP boot.TCP are integers!!!!
-            isInt = checkInteger(token);
-            if(isInt) i++;
-            printf("isInt? %d\n", isInt);
-            args[i] = token;
-            printf("args: %s\n", args[i]);
-          }
-        }
 
         // number of arguments comparison and validating the required integer
-        if(i > nArgs || i < nArgs) printf("Did you mean something like 'entry <i> <boot> <boot.IP> <boot.TCP>'?\n");
-        else if(!isInt) printf("There is no server number!\n");
+        if(!checkCommands(4, token)) printf("Did you mean something like 'entry <i> <boot> <boot.IP> <boot.TCP>'?\n");
         else {
           //do entry server stuff here...
 
-          printf("New server %s was entered!\n", args[0]);
+          printf("The new server was entered!\n");
         }
 
       }
 
       else if(strncmp(token, "sentry", 5) == 0){
-        //do stuff here...
+        if(!checkCommands(4, token)) printf("Did you mean something like 'sentry <i> <succi> <succi.IP> <succi.TCP>'?\n");
+        else {
+          //do entry server stuff here...
 
-        printf("\n");
+          printf("The new server was entered!\n");
+        }
       }
 
       else if(strcmp(token, "leave\n") == 0){
