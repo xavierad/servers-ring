cba5b8a4fb20a5d522f53a08529811bbd09e23e6
5tHorseman <57562498+5tHorseman@users.noreply.github.com>
2020-03-20 12:07:15 +0000

    Update checks.c

--- checks.c
+++ checks.c
@@ -13,12 +13,14 @@ char **args; /* auxiliary array that will contain every arguments after the firs
  * returns: 1 if in the string there is only integers
             0 if there is some letter
 *******************************************************************************/
-int checkInteger(char *num) {
+int checkInteger(char *num) 
+{
 
   int i=0;
   int numDigits = 0;
   /* here we need also to check if there is any '\n' or '\0' for ensurement */
-  while (i < (strlen(num)-1)){
+  while (i < (strlen(num)-1))
+  {
 
     if(isdigit(num[i])) numDigits ++; /* when the number is not in [0, 9] */
     i++;
@@ -27,36 +29,43 @@ int checkInteger(char *num) {
   else return 0;
 }
 
-int checkCommand_NEW_FIND(char* token ) { // mode indicates if the command is new or find
+int checkCommand_NEW_FIND(char* token ) 
+{ // mode indicates if the command is new or find
 
   args = (char**) malloc(3 * sizeof(char*));
   args[0] = token;
 
   int i = 1;
-  for ( ;token = strtok(NULL, " "); token != NULL){
+  for ( ;token = strtok(NULL, " "); token != NULL)
+  {
     args[i] = token;
 
     i++;
-    if(i > 2) {
+    if(i > 2) 
+    {
       printf("Too many arguments!\n");
       break;
       return 0;
     }
   }
 
-  if(i < 2 ) {
+  if(i < 2 ) 
+  {
     printf("Too few arguments!\n");
     return 0;
   }
-  else if( args[1] != NULL ) {
+  else if( args[1] != NULL ) 
+  {
 
-    if(!checkInteger(args[1])) {
+    if(!checkInteger(args[1])) 
+    {
       printf("Second argument is not an integer.\n");
       return 0; /*error*/
     }
     return 1; /*OK*/
   }
-  else {
+  else 
+  {
     printf("There is something wrong with the command line. Try <command> <integer>");
     return 0; /*error*/
   }
@@ -64,14 +73,16 @@ int checkCommand_NEW_FIND(char* token ) { // mode indicates if the command is ne
 
 
 //Checks the inputs for commands entry and sentry
-int checkCommand_S_ENTRY(char* token) {
+int checkCommand_S_ENTRY(char* token) 
+{
 
   args = (char**) malloc(6 * sizeof(char*));
   args[0] = token;
 
 
   int i = 1;
-  for ( ;token = strtok(NULL, " "); token != NULL){
+  for ( ;token = strtok(NULL, " "); token != NULL)
+  {
     args[i] = token;
     printf("token %s\n", token);
 
@@ -83,34 +94,42 @@ int checkCommand_S_ENTRY(char* token) {
     }
   }
 
-  if(i < 5 ) {
+  if(i < 5 ) 
+  {
     printf("Too few arguments!\n");
     return 0;
   }
-  else if( args != NULL ) {
+  else if( args != NULL ) 
+  {
 
-    if(!checkInteger(args[1])) {
+    if(!checkInteger(args[1])) 
+    {
       printf("Second argument is not an integer.\n");
       return 0; /*error*/
     }
-    else if(!checkInteger(args[2])){
+    else if(!checkInteger(args[2]))
+    {
       printf("Second argument is not an integer.\n");
       return 0; /*error*/
     }
-    else if(check_IP(args[3]) == 1){
+    else if(check_IP(args[3]) == 1)
+    {
       printf("IP addresses must be in the form (X)(X)X.(X)(X)X.(X)(X)X.(X)(X)X\n");
       return 0;
     }
-    else if(check_IP(args[3]) == 2){
+    else if(check_IP(args[3]) == 2)
+    {
       printf("IP addresses must contain only numbers\n");
       return 0;
     }
-    else if(!check_Port(args[4])){
+    else if(!check_Port(args[4]))
+    {
       printf("Port must be between (2^10 + 1) and 2^16\n");
       return 0;
     }
   }
-  else {
+  else 
+  {
     printf("There is something wrong with the command line. Try <command> <integer>");
     return 0; /*error*/
   }
@@ -125,7 +144,8 @@ int checkCommand_S_ENTRY(char* token) {
  * returns: 1 if it is in the wright format
             2 otherwise
 *******************************************************************************/
-int check_IP(char* s) {
+int check_IP(char* s) 
+{
 
 
     char *auxs = malloc((strlen(s)+1) * sizeof(char));
@@ -158,14 +178,17 @@ int check_IP(char* s) {
  * returns: 1 if it is in the above interval
             0 otherwise
 *******************************************************************************/
-int check_Port(char* p) {
+int check_Port(char* p) 
+{
 
   /* Port must not contain letters and must be between (2^10 + 1) and 2^16 */
-  if(!checkInteger(p)){
+  if(!checkInteger(p))
+  {
     printf("Port must not contain letters\n\n");
     return 0;
   }
-  else if (atoi(p)<1025 || atoi(p)>65536){
+  else if (atoi(p)<1025 || atoi(p)>65536)
+  {
     printf("Port must be between 1025 and 65536\n\n");
     return 0;
   }
